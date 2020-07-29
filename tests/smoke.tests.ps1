8a839ac693313375228c83ac9bf99197620ad669
Travis Plunk <travis.plunk@microsoft.com>
2020-07-29 14:52:01 -0700

    Update smoke.tests.ps1

--- tests/smoke.tests.ps1
+++ tests/smoke.tests.ps1
@@ -2,4 +2,10 @@ Describe "snap smoke tests" {
     it "we are running the snap" {
         $pshome | Should -Match ".*snap.*" -Because "the path should contain the word snap"
     }
+    it "we can make https calls and parse json" {
+        $r = Invoke-RestMethod -Uri https://aka.ms/pwsh-buildinfo-stable
+        $r.ReleaseDate.gettype().name | Should -Be 'DateTime'
+        $r.BlobName | Should -Match 'v\d-\d-\d'
+        $r.ReleaseTag  | Should -Match 'v\d\.\d\.\d'
+    }
 }
